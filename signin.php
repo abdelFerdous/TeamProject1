e55bb38a3c784e6faa268a64275cee6fcd5d89d4
abdelFerdous <abdel22000@student.hamk.fi>
2023-02-15 20:51:25 +0200

    signinForm

---
+++ signin.php
@@ -0,0 +1,21 @@
+<?php  $stylesheet="signinstyle.css";$title="Sign in" ; include("header.php") ?><br><br>
+<div class="wrapper">
+<div class="login-box">
+    <img src="pic/avatar.png" class="avatar">
+        <h1>Login Here</h1>
+            <form>
+            <p>Username</p>
+            <input type="text" name="username" placeholder="Enter Username">
+            <p>Password</p>
+            <input type="password" name="password" placeholder="Enter Password">
+            <input type="submit" name="submit" value="Login">
+            <a href="#">Forget Password</a>    
+            </form>
+        
+        
+</div>
+</div>
+  </body>
+
+
+
