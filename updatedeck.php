cd35868a6c3d8a586617b2eaae11f3f5843535d5
MattEnricoFlores <mattenricoflores2022@gmail.com>
2023-02-24 10:33:32 +0200

    Added the ability to edit decks

--- updatedeck.php
+++ updatedeck.php
@@ -1,21 +1,29 @@
-<?php $style1="deckstyle.css" ;
-$title = "Deck Database";
-$style2 = "style1.css";
-$game1 = "Legend of Runterra Deck";
+<?php $title = "Deck Updater";
+
+$style1="deckstyle.css";
+$style2 = "style1.css"; 
+$game1 = "Deck Updater";
 $logo = "pic/black.jpg";
-include("header.php") ?>
+include 'deckdb.php';
+include("header.php");
+?>
+<br><br><br>
+<?php
+$a =$_GET['deckName'];
+$result = mysqli_query($conn,"Select * from matt_decks
+where deckName='$a' ");
+$row = mysqli_fetch_array($result);
+?>
 
 
 
 <div class="wrapper">
     <div class="title">
-Enter you Deck here!
+    Update your Deck below! (Name cannot be changed)
     </div>
-<form name="deckform" method="post" action="" onsubmit="return deckval()">
+<form name="deckUpdate" method="post" action="">
     <div class="form-group">
-    <input type="text" name="email" class="form-control" placeholder="Account Email:" required> <br><br>
-    <input type="text" name="deckName" class="form-control" placeholder="Deck Name:" required> <br><br>
-    <input type="text" name="deckCode" class="form-control" placeholder="Deck Code:" required> <br><br>
+    <input type="text" name="deckCode" class="form-control" placeholder="Deck Code:"> <br><br>
     </div>
     <div class="form-group">
     <select name="region1" class="form-control">
@@ -49,48 +57,38 @@ Enter you Deck here!
     </select>
     </div>
     <br>
-    <input type="submit" value="Submit" name="Submit">
+    <input type="submit" value="Update your Deck" name="update"> <br><br>
+    <input type="submit" value="Delete your Deck" name="delete"> <br><br>
 </form>
 </div>
-
 <?php
-if(isset($_POST["Submit"])){
-$email = $_POST['email'];
-$deckName = $_POST['deckName'];
-$deckCode = $_POST['deckCode'];
-$region1 = $_POST['region1'];
-$region2 = $_POST['region2'];
-include 'deckdb.php';
-$sql = "insert into matt_decks (email, deckName, deckCode, region1, region2)
-values('$email', '$deckName', '$deckCode', '$region1', '$region2')";
+if (isset($_POST['update'])){
+    $deckCode = $_POST['deckCode'];
+    $region1 = $_POST['region1'];
+    $region2 = $_POST['region2'];
+    $query = mysqli_query($conn, "UPDATE matt_decks set  
+    deckCode='$deckCode', region1='$region1', region2='$region2' where deckName='$a' ");
 
-if ($conn ->query($sql) === TRUE){
-        echo "<h2>Your information was added successfully</h2>";
+if ($query){
+    echo "<h2> Your Deck was updated successfully</h2>";
 }
 else {
-    echo "<h2>ERROR: Your information was not added successfully. (Deck Names need to be unique)</h2>";
+    echo "Deck was not updated.";
+}
+}
+
+if (isset($_POST['delete'])){
+    $query = mysqli_query($conn, "DELETE FROM matt_decks where deckName='$a' ");
+
+if ($query){
+        echo "<h2> Your Deck was deleted successfully</h2>";
+}
+else {
+    echo "Deck was not deleted.";
 }
 }
 ?>
 
-<div class="wrapper">
-    <div class="title">
-Check Your Decks Here!
-    </div>
-<form name="checkform" method="post" action="deckcheck.php" onsubmit="return checkval()">
-    <div class="form-group">
-    <input type="text" name="checkEmail" class="form-control" placeholder="Account Email:" required> <br><br>
-    </div>
-    <br>
-    <input type="submit" value="Check" name="Check">
-</div>
-
-
-
-
-
-
-</body>
 
 
 <?php include("footer.php") ?>
