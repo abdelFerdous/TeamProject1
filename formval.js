104d085ed7ec458ba6adf44f4b89743a3e66f9f4
abdelFerdous <114020893+abdelFerdous@users.noreply.github.com>
2023-03-06 09:56:33 +0200

    someJsValidation

--- formval.js
+++ formval.js
@@ -55,12 +55,10 @@ return false;
 }
 
 function matchPassword() {  
-    var pw1 = document.getElementById("pswd1");  
-    var pw2 = document.getElementById("pswd2");  
+    var pw1 = document.getElementById("pswd1").value;  
+    var pw2 = document.getElementById("pswd2").value;  
     if(pw1 != pw2)  
     {   
       alert("Passwords did not match");  
-    } else {  
-      alert("Password created successfully");  
     }  
   } 
